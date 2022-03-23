b7f18a7e950228beabab860994f7fd6e20a7f223
suresh.krishnan <skkrishn@berkeley.edu>
2022-03-23 10:13:54 -0700

    added dropdown form

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ var taskToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function() {
     var listItemEl = document.createElement("li");
-    listItemEl.className = "tast-item";
+    listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     taskToDoEl.appendChild(listItemEl);
 }
